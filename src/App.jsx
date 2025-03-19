32bced2089be0d09a6456bf5f6e2ec7323e1c895
OshanAmarathunga <oshanamarathunga1@gmail.com>
2025-03-19 20:14:35 +0530

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Navbar } from './component/navbar/Navbar'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import { darkTheme } from './theme/DarkTheme'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      
-    </>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline/>
+      <Navbar/>
+    </ThemeProvider>
   )
 }
 
